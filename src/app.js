d1de5e4b9dbc28ae7abb793a25f628c5509ca215
Marione Fiszman <marionef1985@gmail.com>
2023-02-22 16:09:57 +0000

    clenar code form comments

--- src/app.js
+++ src/app.js
@@ -10,18 +10,6 @@ window.onload = function() {
   //getDomainNameTest(pronoun, adj, noun, extension);
 };
 
-//const getDomainName = (pronoun, adj, noun, extension) => {
-//pronoun.forEach(pronoun => {
-//adj.forEach(adj => {
-//noun.forEach(noun => {
-//extension.forEach(extension => {
-//const domainName = `${pronoun}${adj}${noun}${extension}`;
-//console.log(domainName);
-//});
-//});
-//});
-//});
-//};
 const pronoun = ["the", "our"];
 const adj = ["great", "big"];
 const noun = ["jogger", "racoon"];
@@ -63,79 +51,3 @@ const getAdj = losPronombres => {
 pronoun.forEach(losPronombres => {
   getAdj(losPronombres);
 });
-
-//getDomainNameFinal(losPronombres, losAdjetivos, losSustantivos, lasExtensiones);
-
-//const getDomainNameTest = (pronoun, adj, noun, extension) => {
-//  const domainName = `${pronoun}${adj}${noun}${extension}`;
-//  console.log(domainName);
-//};
-
-//const getExtension = (noun) => {
-//extension.forEach((extension) => {
-//getDomainNameTest(pronoun, adj, noun, extension);
-//});
-//};
-
-//const getNoun = (adj) => {
-//  noun.forEach((noun) => {
-//    getExtension(noun);
-//  });
-//};
-
-//const getAdj = (pronoun) => {
-//  adj.forEach((adj) => {
-//    getNoun(adj);
-//  });
-//};
-//const getPronoun = (pronoun) => {
-//pronoun.forEach((pronoun) => {
-//  getAdj(pronoun);
-//});
-//};
-
-//const getDomainName = (pronoun, adj, noun, extension) => {
-//  pronoun.forEach((pronoun) => adj;
-//  adj.forEach(getAdj);
-//  noun.forEach(getNoun);
-//  extension.forEach(getExtension);
-//  const domainName = `${pronoun}${adj}${noun}${extension}`;
-//  console.log(domainName);
-//};
-
-//const getPronoun = () => {
-//  const pronoun = ["the", "our"];
-//  pronoun.forEach(getAdj);
-//  return pronoun;
-//};
-//const getAdj = () => {
-// const adj = ["great", "big"];
-//adj.forEach(getNoun);
-//return adj;
-//};
-//const getNoun = () => {
-//  const noun = ["jogger", "racoon"];
-//  noun.forEach(getExtension);
-//  return noun;
-//};
-
-//const getExtension = () => {
-//  const extension = [".com", ".es"];
-//  extension.forEach(getFinal);
-// return extension;
-//};
-
-//const getFinal = (pronoun, adj, noun, extension) => {
-//  const finalDomaineName = `${pronoun}${adj}${noun}${extension}`;
-//  console.log(finalDomaineName);
-//};
-
-//const primerNivel = elementosDePrimerNivel => {
-//  segundoNivel(elementosDePrimerNivel);
-//};
-
-//const segundoNivel = elementosdelPrimerNivel => {
-//  const elementosDelSegundoNivel = [];
-//  tercerNivel(); ////elementosDelSegundoNivel)
-////return elementosdelPrimerNivel+elementosDelSegundoNivel;??
-//};
---
+++ src/myprocess.js
@@ -0,0 +1,86 @@
+//const getDomainName = (pronoun, adj, noun, extension) => {
+//pronoun.forEach(pronoun => {
+//adj.forEach(adj => {
+//noun.forEach(noun => {
+//extension.forEach(extension => {
+//const domainName = `${pronoun}${adj}${noun}${extension}`;
+//console.log(domainName);
+//});
+//});
+//});
+//});
+//};
+
+//const getDomainNameTest = (pronoun, adj, noun, extension) => {
+//  const domainName = `${pronoun}${adj}${noun}${extension}`;
+//  console.log(domainName);
+//};
+
+//const getExtension = (noun) => {
+//extension.forEach((extension) => {
+//getDomainNameTest(pronoun, adj, noun, extension);
+//});
+//};
+
+//const getNoun = (adj) => {
+//  noun.forEach((noun) => {
+//    getExtension(noun);
+//  });
+//};
+
+//const getAdj = (pronoun) => {
+//  adj.forEach((adj) => {
+//    getNoun(adj);
+//  });
+//};
+//const getPronoun = (pronoun) => {
+//pronoun.forEach((pronoun) => {
+//  getAdj(pronoun);
+//});
+//};
+
+//const getDomainName = (pronoun, adj, noun, extension) => {
+//  pronoun.forEach((pronoun) => adj;
+//  adj.forEach(getAdj);
+//  noun.forEach(getNoun);
+//  extension.forEach(getExtension);
+// const domainName = `${pronoun}${adj}${noun}${extension}`;
+//  console.log(domainName);
+//};
+
+//const getPronoun = () => {
+//  const pronoun = ["the", "our"];
+//  pronoun.forEach(getAdj);
+//  return pronoun;
+//};
+//const getAdj = () => {
+// const adj = ["great", "big"];
+//adj.forEach(getNoun);
+//return adj;
+//};
+//const getNoun = () => {
+//  const noun = ["jogger", "racoon"];
+//  noun.forEach(getExtension);
+//  return noun;
+//};
+
+//const getExtension = () => {
+//  const extension = [".com", ".es"];
+//  extension.forEach(getFinal);
+// return extension;
+//};
+
+//const getFinal = (pronoun, adj, noun, extension) => {
+//  const finalDomaineName = `${pronoun}${adj}${noun}${extension}`;
+//  console.log(finalDomaineName);
+//};
+
+//const primerNivel = elementosDePrimerNivel => {
+//  segundoNivel(elementosDePrimerNivel);
+//};
+
+//const segundoNivel = elementosdelPrimerNivel => {
+//  const elementosDelSegundoNivel = [];
+//  tercerNivel(); ////elementosDelSegundoNivel)
+////return elementosdelPrimerNivel+elementosDelSegundoNivel;??
+//};
